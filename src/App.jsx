0e63efe63569b0e13b59450a8212ee661bafb020
Ana Troncoso <ana.selomit.tr@gmail.com>
2024-06-09 00:24:42 -0400

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Router from "./router/Router";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+	return (
+		<>
+			<BrowserRouter>
+				<Navbar />
+				<Router />
+			</BrowserRouter>
+		</>
+	);
+}
+
+export default App;
+
---
+++ src/views/Home.jsx
@@ -0,0 +1,17 @@
+const Home = () => {
+	return (
+		<>
+			<div className="home-container">
+				{" "}
+				<h2>Bienvenido entrenador pokemón</h2>
+				<img
+					src="../public/img/pika1.jpg"
+					alt="Bienvenido a la Pokédex"
+					className="home-pika"
+				/>
+			</div>
+		</>
+	);
+};
+
+export default Home;
